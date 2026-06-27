b3d2d4381f8856e5eb98143fa1f8859c442f4a33
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-06-27 11:16:45 +0300

    feat: Created Team routes

---
+++ backend/src/routes/teamRoutes.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { protect, restrictTo } from "../controllers/authController";
+
+const teamRoutes = express.Router();
+
+teamRoutes.use(protect);
+
+teamRoutes.route("/");
+// POST create a team,
+// GET all teams restrictTo("admin", "superAdmin")
+
+teamRoutes.route("/my-team");
+// GET my team
+// DELETE my team
+
+teamRoutes.route("/join"); // POST join a team
+teamRoutes.route("/leave"); // DELETE leave a team
+
+// NOTE: All next routes for captain only
+teamRoutes.route("/:id/name"); // PATCH change team name for
+
+teamRoutes.route("/:id/captain"); // PATCH change team captain
+
+teamRoutes.route("/:id/members/:userId"); // DELETE kick a member
+
+// NOTE: Admins only
+teamRoutes.route("/:id"); // GET any team
+
+export default teamRoutes;
