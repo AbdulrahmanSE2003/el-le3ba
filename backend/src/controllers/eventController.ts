4bba0b2ba465fbcbc7940327cd8469b3725fcb08
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-06-28 02:34:42 +0300

    feat: Finished event controller logic with test

--- backend/src/controllers/eventController.ts
+++ backend/src/controllers/eventController.ts
@@ -8,6 +8,7 @@ import {
   getOne,
   updateOne,
 } from "../utils/factory";
+import resHandler from "../utils/resHandler";
 
 export const getCurrentEvent = catchAsync(async (req, res, next) => {
   const event = await Event.findOne({ status: "running" });
@@ -15,9 +16,24 @@ export const getCurrentEvent = catchAsync(async (req, res, next) => {
   resHandler(res, 200, "event", event);
 });
 
-export const getAllEvents = getAll(Event);
+export const getAllEvents = getAll(Event, {
+  path: "User",
+  select: "name email",
+});
 
-export const createEvent = createOne(Event);
+export const createEvent = catchAsync(async (req, res, next) => {
+  if (!req.user) return next(new AppError("Not authenticated.", 401));
+
+  const newEvent = await Event.create({
+    title: req.body.title,
+    createdBy: req.user._id,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+    maxAttempts: req.body.maxAttempts,
+  });
+
+  resHandler(res, 201, "newEvent", newEvent);
+});
 
 export const getEvent = getOne(Event);
 
