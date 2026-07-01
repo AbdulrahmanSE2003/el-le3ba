36bdbe2075f322dc423caee2a0ee88af5819871a
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-07-01 17:43:41 +0300

    feat: Implemented getting my rank in backend

--- frontend/features/match/components/Lobby.tsx
+++ frontend/features/match/components/Lobby.tsx
@@ -4,7 +4,6 @@ import StartMatch from "@/features/match/components/StartMatch";
 import RulesSection from "./RulesSection";
 import { Event, Member, Team } from "../types";
 import { useUserStore } from "@/store/userStore";
-import { Hourglass } from "lucide-react";
 
 const Lobby = ({
   event,
@@ -39,14 +38,44 @@ const Lobby = ({
       {isCaptain ? (
         <StartMatch />
       ) : (
-        <div className={`w-full border-dashed border-accent`}>
-          <Hourglass />
+        <div
+          className={`w-full bg-accent/15 rounded-lg border-2 border-dashed border-accent/50 p-3 flex items-center justify-center`}
+        >
           <p>قول للكابتن يبدأ بسرعة 😶</p>
         </div>
       )}
 
       {/* Game Rules */}
       <RulesSection />
+
+      {/* Team Stats */}
+      <div className={`w-full grid md:grid-cols-2 gap-3`}>
+        {/* Total Points */}
+        <div
+          className={`w-full flex flex-col items-center gap-y-4 border border-primary/30 bg-primary/15 " +
+      "dark:border-primary/20 dark:bg-primary/5 rounded-lg p-4`}
+        >
+          <span className={`text-xs text-muted-foreground`}>مجموع النقاط</span>
+          <span className={`text-5xl text-accent font-display font-bold`}>
+            {" "}
+            {team.team.points}
+          </span>
+        </div>
+
+        {/* Rank */}
+        <div
+          className={`w-full flex flex-col items-center gap-y-4 border border-primary/30 bg-primary/15 " +
+      "dark:border-primary/20 dark:bg-primary/5 rounded-lg p-4`}
+        >
+          <span className={`text-xs text-muted-foreground`}>
+            الترتيب الحالي
+          </span>
+          <span className={`text-5xl text-accent font-display font-bold`}>
+            {" "}
+            {team.team.points}
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
