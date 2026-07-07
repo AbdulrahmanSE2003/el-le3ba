8dd568224f5a5e61d577c54b0c664559bd224953
Abdoh <abdohmahmoud28@gmail.com>
2026-07-07 04:46:41 +0300

    fix(profile): fix save password button and add loading state

--- frontend/features/profile/components/edit-profile/SavePasswordBtn.tsx
+++ frontend/features/profile/components/edit-profile/SavePasswordBtn.tsx
@@ -9,6 +9,7 @@ interface Props {
 export default function SavePasswordBtn({ isPending }: Props) {
   return (
     <Motion
+      as="button"
       type="submit"
       whileTap={{ scale: 0.97 }}
       className="flex-1 h-9 rounded-xl bg-primary text-primary-foreground text-sm font-bold hover:bg-primary/90 flex items-center justify-center gap-2 cursor-pointer duration-200 disabled:opacity-50"
