02240a88cce35cce2ad23ef7a62c4c58cc79d9a4
AbdulrahmanSE2003 <abdulrahman.saad2303@gmail.com>
2026-07-10 14:45:50 +0300

    feat(): implemented

--- frontend/features/dashboard/components/Notification.tsx
+++ frontend/features/dashboard/components/Notification.tsx
@@ -1,14 +1,48 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import { Button } from "@/components/ui/button";
 import { Bell } from "lucide-react";
 
 const Notification = () => {
   return (
-    <div
-      className={`hover:bg-muted p-2 rounded-full transition-colors duration-300 cursor-pointer group`}
-    >
-      <Bell
-        className={`text-muted-foreground/90 size-4.5 group-hover:text-muted-foreground`}
-      />
-    </div>
+    <Drawer direction="right">
+      <DrawerTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className={`hover:bg-muted p-2 rounded-full transition-colors duration-300 cursor-pointer group`}
+        >
+          <Bell
+            className={`text-muted-foreground/90 size-4.5 group-hover:text-muted-foreground`}
+          />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle
+            className={`text-right text-xl font-semibold text-primary`}
+          >
+            قائمة الإشعارات
+          </DrawerTitle>
+          <DrawerDescription>This action cannot be undone.</DrawerDescription>
+        </DrawerHeader>
+        <div className="p-4">{/* Content here */}</div>
+        <DrawerFooter>
+          <Button>تعليم الكل كمقروءة</Button>
+          <DrawerClose asChild>
+            <Button variant="outline">إلغاء</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
