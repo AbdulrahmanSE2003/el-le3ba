77865b22ba0e096a18df13b591959821c7232067
Abdoh <abdohmahmoud28@gmail.com>
2026-07-21 19:48:26 +0300

    merge: merge team branch into dev

--- frontend/features/team-profile/components/TeamHeader.tsx
+++ frontend/features/team-profile/components/TeamHeader.tsx
@@ -1,5 +1,4 @@
 import { Crown, Trophy, Edit2, Trash2, Calendar } from "lucide-react";
-import { TeamProfileData } from "../types";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
