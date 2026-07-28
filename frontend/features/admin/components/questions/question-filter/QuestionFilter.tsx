3adc46d89402f96e9d6eac1b8aab1ea5191028eb
Abdoh <abdohmahmoud28@gmail.com>
2026-07-29 00:20:08 +0300

    style: refine admin questions UI

--- frontend/features/admin/components/questions/question-filter/QuestionFilter.tsx
+++ frontend/features/admin/components/questions/question-filter/QuestionFilter.tsx
@@ -3,7 +3,7 @@ import FiltersContainer from "./FiltersContainer";
 
 export default function QuestionFilter() {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-between gap-4 bg-white p-5 rounded-lg">
+    <div className="flex flex-col lg:flex-row items-center justify-between gap-4 bg-white dark:bg-card p-5 rounded-lg">
       <SearchInput placeholder="أبحث في نص السؤال..." />
 
       <FiltersContainer />
